d5dd561abfdfbcef61a277a90152a53c308c6004
akvasov <35040802+akvasov@users.noreply.github.com>
2023-07-30 11:44:27 -0400

    terraform supply file

---
+++ terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  cloud {
+    organization = "example-org-46795e"
+
+    workspaces {
+      name = "oci-terraform-github-actions"
+    }
+  }
+
+    resource "null_resource" "example" {
+       triggers = {
+         value = "A example resource that does nothing!"
+       }
+     }
+
+
+}
